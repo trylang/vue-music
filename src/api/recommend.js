f91d0b9f1e8a0a63695ceca692ca19392aa6c248
Jane <trylang@foxmail.com>
2017-09-13 21:00:40 +0800

    添加scroll

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,4 +1,5 @@
 import jsonp from 'common/js/jsonp'
+import axios from 'axios'
 import {
   commonParams,
   options
@@ -15,4 +16,26 @@ export function getRecommend() {
   })
 
   return jsonp(url, data, options)
+}
+
+export function getDiscList() {
+  const url = '/api/getDiscList'
+
+  const data = Object.assign({}, commonParams, {
+    rnd: Math.random(),
+    hostUin: 0,
+    format: 'json',
+    platform: 'yqq',
+    needNewCode: 0,
+    categoryId: 10000000,
+    sortId: 5,
+    sin: 0,
+    ein: 29
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -5,7 +5,7 @@
       </slot>
     </div>
     <div class="dots">
-      <span class="dot"></span>
+      <span class="dot" :class="{active: currentPageIndex === index}" v-for="(item,index) in dots"></span>
     </div>
   </div>
 </template>
@@ -33,13 +33,25 @@ export default {
   data () {
     return {
       dots: [],
-      currentIndex: 0
+      currentPageIndex: 0
     }
   },
   mounted() {
     setTimeout(() => {
       this._setSliderWidth()
       this._initSlider()
+      this._initDots()
+      if (this.loop) {
+        this._play()
+      }
+    }, 20)
+
+    window.addEventListener('resize', () => {
+      if (!this.slider) {
+        return
+      }
+      this._setSliderWidth(true)
+      this.slider.refresh()
     })
   },
   methods: {
@@ -48,14 +60,57 @@ export default {
         scrollX: true,
         scrollY: false,
         momentum: false,
-        snap: true,
-        snapLoop: this.loop,
-        snapThreshold: 0.3,
+        snap: {
+          loop: this.loop,
+          threshold: 0.3
+        },
         snapSpeed: 400
       })
+
+      let that = this.slider
+      this.slider.on('scrollEnd', () => {
+        let pageIndex = that.currentPage.pageX
+        if (this.loop) {
+          pageIndex -= 1
+        }
+        this.currentPageIndex = pageIndex
+        if (this.loop) {
+          this._play()
+        }
+      })
+
+      this.slider.on('beforeScrollStart', () => {
+        if (this.autoPlay) {
+          clearTimeout(this.timer)
+        }
+      })
     },
-    _setSliderWidth() {
+    _setSliderWidth(isResize) {
       this.children = this.$refs.sliderGroup.children
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i]
+        child.style.width = sliderWidth + 'px'
+        width += sliderWidth
+        addClass(child, 'slider-item')
+      }
+      if (this.loop && !isResize) {
+        width += 2 * sliderWidth
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initDots() {
+      this.dots = new Array(this.children.length - 2)
+    },
+    _play() {
+      let pageIndex = this.currentPageIndex + 1
+      if (this.loop) {
+        pageIndex += 1
+      }
+      this.timer = setTimeout(() => {
+        this.slider.goToPage(pageIndex, 0, 400)
+      }, this.interval)
     }
   },
   components: {
@@ -64,5 +119,44 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .slider
+    min-height: 1px
+    .slider-group
+      position: relative
+      overflow: hidden
+      white-space: nowrap
+      .slider-item
+        float: left
+        box-sizing: border-box
+        overflow: hidden
+        text-align: center
+        a
+          display: block
+          width: 100%
+          overflow: hidden
+          text-decoration: none
+        img
+          display: block
+          width: 100%
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      text-align: center
+      font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
